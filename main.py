ddbac976c6c08ab3323fbf2bc6368f2a651fb00f
dtyafa <kmm15072011@gmail.com>
2024-03-06 08:20:05 +0200

    LVL2

--- main.py
+++ main.py
@@ -1,22 +1,21 @@
 import pygame
-from data import *
+import data
 from maze import *
 
 pygame.init()
 window = pygame.display.set_mode((setting_win["Width"], setting_win["Height"]))
 pygame.display.set_caption("maze")
 
+
 def run():
     bg = pygame.image.load("sprites\\bg.png")
     key_png = pygame.image.load("sprites\\Key.png")
     game = True
     clock = pygame.time.Clock()
-    
-    hero = Hero(45, 10, 60, 136, hero_list, 5)
-    enemy = Enemy(255, 0, 94, 102, robot_list, 2)
-    #enemy2 = Enemy(0, 0, 94, 102, robot_list, 3)
-
-    
+    hero = Hero(45, 10, 60, 120, hero_list, 4, 1)
+    enemy = Enemy(170, 100, 94, 102, robot_list, 4, "x")
+    key = Key(175, 420, 69, 30, key_png)
+    hero.loadLVL()
 
     while game:
         pygame.draw.rect(window, (255, 255, 0), (100, 100, 100, 100))
@@ -24,51 +23,68 @@ def run():
 
         hero.move()
         window.blit(hero.IMAGE, (hero.x, hero.y))
-        if key != True:
-            a = window.blit(key_png, (175,340))
-        enemy.move(255, 0, 255, 180, "y")
+        if hero.LVL == 1:
+            enemy.move(170, 0, 906, 180)
+        elif hero.LVL == 2:
+            enemy.move(0, 102, 180, 898)
         window.blit(enemy.IMAGE, (enemy.x, enemy.y))
-        #enemy2.move()
-        #window.blit(enemy2.IMAGE, (enemy2.x, enemy2.y))
-        for wall in wall_list:
+        if key != 0:
+            window.blit(key.IMAGE, (key.x, key.y))
+        for EXIT in data.exit_list:
+            pygame.draw.rect(window, EXIT.COLOR, EXIT)
+            if EXIT.colliderect(hero):
+                hero = Hero(45, 10, 60, 136, hero_list, 4, hero.LVL + 1)
+                hero.deletewalls(4)
+                enemy = Enemy(850, 100, 94, 102, robot_list, 4, "y")
+                key = Key(900, 100, 69, 30, key_png)
+                hero.lose()
+                enemy.lose()
+        for wall in data.wall_list:
             pygame.draw.rect(window, wall.COLOR, wall)
-        for door in door_list:
+        for door in data.door_list:
             pygame.draw.rect(window, door.COLOR, door)
-
-
-        
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 game = False
             if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_w:
+                if event.key == pygame.K_w or event.key == pygame.K_UP:
                     hero.MOVE["UP"] = True
-                if event.key == pygame.K_s:
+                if event.key == pygame.K_s or event.key == pygame.K_DOWN:
                     hero.MOVE["DOWN"] = True
-                if event.key == pygame.K_a:
+                if event.key == pygame.K_a or event.key == pygame.K_LEFT:
                     hero.MOVE["LEFT"] = True
-                if event.key == pygame.K_d:
+                if event.key == pygame.K_d or event.key == pygame.K_RIGHT:
                     hero.MOVE["RIGHT"] = True
+                if event.key == pygame.K_g:
+                    hero.GODMODE = True
+                    print("GODMODE true")
             if event.type == pygame.KEYUP:
-                if event.key == pygame.K_w:
+                if event.key == pygame.K_w or event.key == pygame.K_UP:
                     hero.MOVE["UP"] = False
-                if event.key == pygame.K_s:
+                if event.key == pygame.K_s or event.key == pygame.K_DOWN:
                     hero.MOVE["DOWN"] = False
-                if event.key == pygame.K_a:
+                if event.key == pygame.K_a or event.key == pygame.K_LEFT:
                     hero.MOVE["LEFT"] = False
-                if event.key == pygame.K_d:
+                if event.key == pygame.K_d or event.key == pygame.K_RIGHT:
                     hero.MOVE["RIGHT"] = False
+                if event.key == pygame.K_g:
+                    hero.GODMODE = False
+                    print("GODMODE false")
 
-        if hero.colliderect(enemy):
+        if hero.colliderect(enemy) and hero.GODMODE == False:
             hero.lose()
-        #    enemy2.lose()
             enemy.lose()
-        if hero.colliderect(a):
-            key_up()
-            
+            if hero.LVL == 1:
+                key = Key(175, 420, 69, 30, key_png)
+            elif hero.LVL == 1:
+                key = Key(900, 100, 69, 30, key_png)
+        if key != 0 and hero.colliderect(key):
+            hero.HAVE_KEY = True
+            key = 0
 
         clock.tick(80)
         pygame.display.flip()
+
 
 run()
--- maze.py
+++ maze.py
@@ -1,9 +1,8 @@
-import pygame
 from data import *
+import pygame
 
 pygame.init()
-global key
-key = False
+
 
 class Sprite(pygame.Rect):
     def __init__(self, x, y, width, height, image_list, speed):
@@ -20,84 +19,100 @@ class Sprite(pygame.Rect):
         self.COUNT_IMG += 1
         if self.COUNT_IMG // 10 == self.COUNT_IMG / 10:
             self.IMAGE = self.IMAGE_LIST[self.COUNT_IMG // 10 - 1]
-        
-
-
 
 
 class Hero(Sprite):
-    def __init__(self, x, y, width, height, image_list, speed):
-        super().__init__(x, y, width, height, image_list, speed) 
-        self.i = 0
-        self.int = 0
+    def __init__(self, x, y, width, height, image_list, speed, lvl):
+        super().__init__(x, y, width, height, image_list, speed)
         self.start_x = x
+        self.HAVE_KEY = False
+        self.GODMODE = False
+        self.LVL = lvl
         self.start_y = self.y
         self.MOVE = {"UP": False, "DOWN": False, "LEFT": False, "RIGHT": False}
-        
+
     def move(self):
         if self.MOVE["UP"]:
             self.y -= self.SPEED
             self.move_image(len(self.IMAGE_LIST) * 10 + 9)
-            if self.collidelist(wall_list) != -1:
+            if self.collidelist(wall_list) != -1 and self.GODMODE == False or (
+                    self.collidelist(door_list) != -1 and self.HAVE_KEY == False):
                 self.y += self.SPEED
-            if key != True:
-                if self.collidelist(door_list) != -1:
-                    self.y += self.SPEED
         elif self.MOVE["DOWN"]:
             self.y += self.SPEED
             self.move_image(len(self.IMAGE_LIST) * 10 + 9)
-            if self.collidelist(wall_list) != -1:
+            if self.collidelist(wall_list) != -1 and self.GODMODE == False or  (
+                    self.collidelist(door_list) != -1 and self.HAVE_KEY == False):
                 self.y -= self.SPEED
-            if key != True:
-                if self.collidelist(door_list) != -1:
-                    self.y -= self.SPEED
         elif self.MOVE["LEFT"]:
             self.x -= self.SPEED
             self.move_image(len(self.IMAGE_LIST) * 10 + 9)
-            if self.collidelist(wall_list) != -1:
+            if self.collidelist(wall_list) != -1 and self.GODMODE == False or (
+                    self.collidelist(door_list) != -1 and self.HAVE_KEY == False):
                 self.x += self.SPEED
-            if key != True:
-                if self.collidelist(door_list) != -1:
-                    self.x += self.SPEED
         elif self.MOVE["RIGHT"]:
             self.x += self.SPEED
             self.move_image(len(self.IMAGE_LIST) * 10 + 9)
-            if self.collidelist(wall_list) != -1:
+            if self.collidelist(wall_list) != -1 and self.GODMODE == False or (
+                    self.collidelist(door_list) != -1 and self.HAVE_KEY == False):
                 self.x -= self.SPEED
-            if key != True:
-                if self.collidelist(door_list) != -1:
-                    self.x -= self.SPEED
-        if self.x >= (setting_win["Width"] - 74): 
+        if self.x >= (setting_win["Width"] - 74):
             self.x = self.x - self.SPEED
-        if self.x <= -1: 
+        if self.x <= -1:
             self.x = self.x + self.SPEED
-        if self.y >= (setting_win["Height"] - 175): 
+        if self.y >= (setting_win["Height"] - 125):
             self.y = self.y - self.SPEED
-        if self.y <= -2: 
+        if self.y <= -2:
             self.y = self.y + self.SPEED
-        
-
 
     def lose(self):
         self.x = self.start_x
         self.y = self.start_y
-    
+        self.loadLVL()
+
+    def loadLVL(self):
+        if self.LVL == 1:
+            create_wall("MAP1")
+        if self.LVL == 2:
+            create_wall("MAP2")
+
+    def deletewalls(self, index):
+        for i in range(index):
+            for obj in wall_list:
+                wall_list.remove(obj)
+            for obj in door_list:
+                door_list.remove(obj)
+            for obj in exit_list:
+                exit_list.remove(obj)
+
+
+class Key(Sprite):
+    def __init__(self, x, y, width, height, image):
+        super().__init__(x, y, width, height, [0, 1, 2, 3], 0)
+        self.start_x = x
+        self.start_y = self.y
+        self.IMAGE = image
+
+
 class Enemy(Sprite):
-    def __init__(self, x, y, width, height, image_list, speed):
+    def __init__(self, x, y, width, height, image_list, speed, key):
         super().__init__(x, y, width, height, image_list, speed)
+        self.KEY = key
         self.start_x = self.x
         self.start_y = self.y
         self.GOBACK = False
-    def move(self, x1, y1, x2, y2, key):
-        if key == "x":
-            if self.x  < x1 or self > x2:
+
+    def move(self, x1, y1, x2, y2):
+        if self.KEY == "x":
+            if self.x < x1 or self.x > x2:
                 self.SPEED *= -1
             self.x += self.SPEED
-        if key == "y":
-            if self.y  < y1 or self.y > y2:
+        if self.KEY == "y":
+            if self.y < y1 or self.y > y2:
                 self.SPEED *= -1
             self.y += self.SPEED
         self.move_image(len(self.IMAGE_LIST) * 10 + 9)
+
     def lose(self):
         self.x = self.start_x
         self.y = self.start_y
@@ -108,22 +123,22 @@ class Wall(pygame.Rect):
         super().__init__(x, y, width, height)
         self.COLOR = color
 
-door_list = []                                  
+
 def create_wall(key):
     x, y = 0, 0
     x1, y1 = 0, 0
-    step =setting_win["Width"] // 20
+    step = setting_win["Width"] // 20
 
     index_h = 0
     index_v = 0
     for string in maps[key]:
         for element in string:
-            #vertical
+            # vertical
             if element == "1":
                 index_temp = index_v
                 while True:
                     if maps[key][index_temp][index_h] == "2":
-                        wall_list.append(Wall(x, y, 20, y1, (0, 204, 204)))
+                        wall_list.append(Wall(x, y, 20, y1+50, (0, 204, 204)))
                         break
                     y1 += step
                     index_temp += 1
@@ -132,7 +147,7 @@ def create_wall(key):
                 index_temp = index_v
                 while True:
                     if maps[key][index_temp][index_h] == "4":
-                        wall_list.append(Wall(x, y, 20, y1, (0, 204, 204)))
+                        wall_list.append(Wall(x, y-20, 20, y1, (0, 204, 204)))
                         break
                     y1 += step
                     index_temp += 1
@@ -146,7 +161,16 @@ def create_wall(key):
                     y1 += step
                     index_temp += 1
                 y1 = 0
-            #horizontal
+            if element == "e":
+                index_temp = index_v
+                while True:
+                    if maps[key][index_temp][index_h] == "t":
+                        exit_list.append(Wall(x+30, y+30, 20, y1+20, (172, 169, 255)))
+                        break
+                    y1 += step
+                    index_temp += 1
+                y1 = 0
+            # horizontal
             if element == "3":
                 index_temp = index_h
                 while True:
@@ -166,24 +190,19 @@ def create_wall(key):
                     index_temp += 1
                 x1 = 0
 
-            if element == "s":
+            if element == "d":
                 index_temp = index_h
                 while True:
-                    if string[index_temp] == "e":
+                    if string[index_temp] == "r":
                         door_list.append(Wall(x - step, y - 20, x1 + step * 2, 20, (153, 76, 0)))
                         break
                     x1 += step
                     index_temp += 1
                 x1 = 0
+
             index_h += 1
             x += step
         y += step
         x = 0
         index_v += 1
         index_h = 0
-
-def key_up():
-    key = True
-
-create_wall("MAP1")
-    
